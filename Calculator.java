92db1900b56b2a68100320d6433bff4775106a1f
Sagan Maxim <sagan_maxim@mail.ru>
2018-09-28 16:41:19 +0300

    Калькулятор

---
+++ Calculator.java
@@ -0,0 +1,25 @@
+public class Calculator {
+	public static void main(String[] args) {
+		int x = 15;
+		int y = 7;
+		char mathFunction = '%';
+
+		if (mathFunction == '+') {
+			System.out.println (x+y);
+		} else if (mathFunction == '-') {
+			System.out.println (x-y);
+		} else if (mathFunction == '*') {
+			System.out.println (x*y);
+		} else if (mathFunction == '/') {
+			System.out.println (x/y);
+		} else if (mathFunction == '%') {
+			int result = 1;
+			for (int i=1; i<= y; i++) {
+				result *= x;
+			}
+			System.out.println(result);
+		} else if (mathFunction == '%') {
+			System.out.println(x % y);
+		}
+	}
+}
