a65604ad0e305c282e98306754caec7f663a401a
Sagan Maxim <sagan_maxim@mail.ru>
2018-09-28 16:43:37 +0300

    Юникод

---
+++ MyUnicode.java
@@ -0,0 +1,7 @@
+public class MyUnicode {
+	public static void main(String[] args) {
+		for (int i= 33; i<= 126; i++) {
+		System.out.print ((char) i + "  "); // Пробелы для читабельности в консоли
+			}
+}
+}
