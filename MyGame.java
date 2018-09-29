1196b0914c80263c3e598da0834c3a166bb6a375
Sagan Maxim <sagan_maxim@mail.ru>
2018-09-29 20:18:03 +0300

    Сдвинул

--- MyGame.java
+++ MyGame.java
@@ -2,17 +2,17 @@ import java.util.Scanner; // Было в задачах в материалах 
 
 public class MyGame {
 	public static void main(String[] args) {
-	int secretNumber = 25;
+	    int secretNumber = 25;
 	System.out.print("Введи число от 0 до 100");
 	Scanner answer = new Scanner(System.in);
 	int userNumber = answer.nextInt();
 
 	if(userNumber == secretNumber) {
-	System.out.println ("Ты угадал число!");
+	    System.out.println ("Ты угадал число!");
 	} else if(userNumber < secretNumber && userNumber >=0) {
-	System.out.println ("Мое число больше");
+	    System.out.println ("Мое число больше");
 	} else if(userNumber > secretNumber && userNumber <=0) {
-	System.out.println ("Мое число меньше");
-	}
+	    System.out.println ("Мое число меньше");
+	    }
 	}
 }
