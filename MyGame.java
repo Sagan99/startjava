39611da68566702a362373dd2f9ae31460ae3884
Sagan Maxim <sagan_maxim@mail.ru>
2018-09-29 16:55:33 +0300

    Исправил недостатки

--- MyGame.java
+++ MyGame.java
@@ -1,18 +1,18 @@
-import java.util.Scanner; // Было в задачах в материалах про ветвление в Java
+import java.util.Scanner; // Было в задачах в материалах про ветвление Java
 
 public class MyGame {
-public static void main(String[] args) {
-int secretNumber = 76;
-System.out.print("Введи число от 0 до 100: "); 
-Scanner answer = new Scanner(System.in); 
-int userNumber = answer.nextInt(); 
+	public static void main(String[] args) {
+	int secretNumber = 25;
+	System.out.print("Введи число от 0 до 100");
+	Scanner answer = new Scanner(System.in);
+	int userNumber = answer.nextInt();
 
-if(userNumber == secretNumber) {
-System.out.println("Ты угадал. Ты сильное звено!");
-} else if(userNumber < secretNumber && userNumber >= 0) {
-System.out.println("Мое число больше");
-} else if(userNumber > secretNumber && userNumber <= 100) {
-System.out.println("Мое число меньше");
-}
-}
+	if(userNumber == secretNumber) {
+	System.out.println ("Ты угадал число!");
+	} else if(userNumber < secretNumber && userNumber >=0) {
+	System.out.println ("Мое число больше");
+	} else if(userNumber > secretNumber && userNumber <=0) {
+	System.out.println ("Мое число меньше");
+	}
+	}
 }
