63e47a10e2737a915f3dd6ded62842e29446fb8a
Sagan Maxim <sagan_maxim@mail.ru>
2018-09-29 19:22:15 +0300

    Исправил еще раз

--- MyUnicode.java
+++ MyUnicode.java
@@ -1,6 +1,6 @@
 public class MyUnicode {
 	public static void main (String[] args){
-	    for (int i= 33; i<= 126; i++) {
+	    for (int i = 33; i <= 126; i++) {
 	       System.out.print ((char) i + "   "); // Пробелы для читабельности
 	    }
 	}
