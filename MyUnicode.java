094357e2432f9a4df8af6b08ad355f64f1750254
Sagan99 <43201886+Sagan99@users.noreply.github.com>
2018-10-12 13:46:11 +0300

    ГОТОВА

--- MyUnicode.java
+++ MyUnicode.java
@@ -1,5 +1,5 @@
 public class MyUnicode {
-	public static void main (String[] args){
+	public static void main (String[] args) {
 	    for (int i = 33; i <= 126; i++) {
 	       System.out.print ((char) i + "   "); // Пробелы для читабельности
 	    }
